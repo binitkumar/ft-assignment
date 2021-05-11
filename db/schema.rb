2cf0250b6aaf84bcd813cbce36837b897793e8ea
Binit Kumar <binit.kumar.hbs@gmail.com>
2021-05-11 17:49:57 +0530

    Implemented PG for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_11_060650) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "article_url"
     t.string "video_id"
